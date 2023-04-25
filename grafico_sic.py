cd1e360dac09f2883de1f9cfaecd7e276613ccea
Yuripds <yuripedro3@gmail.com>
2023-04-25 12:52:39 -0300

    ultima modificação do sic

--- grafico_sic.py
+++ grafico_sic.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from matplotlib import ticker
+from scipy import special
+
+import modulacao_grafico as mgraf
 
 
 sns.set()
@@ -474,36 +477,54 @@ def get_index_vc(vect):
     split_list.append(len(vect))
     return split_list
 
-def gerando_sinal(dataframe):
-    ruido = 1
-    sobreposicao = 1
-    y = dataframe.iloc[-1].ganho* sobreposicao + ruido
-    sinal_montado = 1
 
+def gerando_sinal(dataframe,SNR_dB):
+    
+    simbolos_tx = []
+    for i in range(len(dataframe)):
+       sinal_com_ruido = mgraf.plot_M_QAM(4,N=100,SNR_dB=SNR_dB) 
+       simbolos_tx.append(abs(sinal_com_ruido).tolist())
+
+    sinal_montado = []
+    for m in range(len(simbolos_tx[0])) :
+        usuarios = []
+        for j in simbolos_tx:
+            usuarios.append(j[m])
+        sinal_montado.append(dataframe.iloc[-1].ganho*sum(np.multiply(usuarios,dataframe.potencia).tolist()))
 
     return sinal_montado
 
 
-def sinal_tx(dataframe):
+# um simbolo por vez?
+def sinal_tx(dataframe,SNR_dB):
     indices = get_index_vc(dataframe.index)
 
     sinal=[]
     init=0
     for i in range(len(indices)-1):
         df_split = dataframe.iloc[indices[init]:indices[init+1]]
-        sinal.append(gerando_sinal(df_split))
+        sinal.append(gerando_sinal(df_split,SNR_dB=SNR_dB))
         init = init + 1
 
     return sinal
 
 
 
+#### de 0 até 25 db de SNR
+SNR = [0,5,10,15,20,25]
 
-sinal= sinal_tx(parametros_df_geral)
+##################### mudar a estrutura de dados de tx_list
+tx_list = []
+for i in SNR:
+    tx_list.append(sinal_tx(parametros_df_geral,SNR_dB=i))
 
 ############################################## calculo da BER #################################################################
 
+########### implementar o SIC
+def sic(sinal):
+    sinal_separado = 1
 
+    return sinal_separado
 
 
 
@@ -519,6 +540,6 @@ sinal= sinal_tx(parametros_df_geral)
 ################ Próximos passos 
 #    1-  Gerar csv com ganho complexo e coeficiente de potencia dos usuários do cluster 0  ------------------ Feito
 #    2-  Implementar modulação 4-QAM ------------------------------------------------------------------------ Feito
-#    3-  Implentar o sinal recebido para diferentes valores de SINR (0 a 25 dB) ----------------------------- Fazendo
-#    4-  Calculo da BER (tem que implementar o SIC) --------------------------------------------------------- 
+#    3-  Implentar o sinal recebido para diferentes valores de SINR (0 a 25 dB) ----------------------------- Feito
+#    4-  Calculo da BER (tem que implementar o SIC) --------------------------------------------------------- Fazendo
 #    5-  2 Gráficos 2D do meu método e do método tradicional.  BER x SINR ----------------------------------- 
