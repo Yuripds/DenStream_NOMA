c35183c00262df6640884bdf74f28fd585efc71c
Yuripds <yuripedro3@gmail.com>
2023-05-11 11:48:11 -0300

    correção de bugs

--- desempenho_sic/grafico_sic.py
+++ desempenho_sic/grafico_sic.py
@@ -88,7 +88,7 @@ def gerar_sinais_function(potencias, SNR_dB):
         simbolos_aux = []
         for usuarios in range(len(i[0])):
             if (i[0][usuarios]) > 0:
-                simbolos_aux.append(mgraf.plot_M_QAM(M=4, N=10000))
+                simbolos_aux.append(mgraf.plot_M_QAM(M=4, N=1000))
 
         conjunto_de_simbolos.append(simbolos_aux)
   
@@ -207,7 +207,7 @@ def SER(sinal_recebido,sinal_enviado):
 
 ##############################################  chamada das funções #############################################################
 
-snr_vect = [0,5,10]
+snr_vect = [100,5,10]
 erro_p_snr = []
 
 for snr in snr_vect:
